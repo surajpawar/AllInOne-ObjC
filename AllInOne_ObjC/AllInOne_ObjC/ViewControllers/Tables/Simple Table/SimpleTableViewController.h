cb715eab380ef9150bdf881419a7914918734af7
Suraj Pawar <surajpawar898@gmail.com>
2017-04-25 22:24:26 +0530

    Simple Table commit

---
+++ AllInOne_ObjC/AllInOne_ObjC/ViewControllers/Tables/Simple Table/SimpleTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SimpleTableViewController.h
+//  AllInOne_ObjC
+//
+//  Created by Suraj Pawar on 25/04/17.
+//  Copyright © 2017 Suraj Pawar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimpleTableViewController : UITableViewController
+
+@end
