21971e64148c0c7826b5ccc8e21943d787219d6a
Suraj Pawar <surajpawar898@gmail.com>
2017-05-02 06:26:56 +0530

    Changes as on 2 May 17, 6:30am
    - Added instructions file.

--- AllInOne_ObjC/AllInOne_ObjC/SimpleTableViewController.m
+++ AllInOne_ObjC/AllInOne_ObjC/SimpleTableViewController.m
@@ -19,12 +19,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    
     marr_TutorialsList = [[NSMutableArray alloc] initWithObjects:@"Objective-C basic", nil];
 }
 
